39d2d3fcc368f9198f1d94b110ca2a81d6939e96
apiepho <alpiepho@gmail.com>
2019-07-19 17:26:37 -0600

    added Google Analytics ID

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   siteTitle: 'State of Javascript 2018 and State of CSS 2019',
   siteDescription: `D3 forced directed diagram based on State of Javascript 2018 and State of CSS 2019 - Ported to React/Gatsby/PWA.`,
   siteFaviocon: './images/favicon.ico',
-  googleAnalyticsId: 'UA-142308652-4',
+  googleAnalyticsId: 'UA-142308652-5',
   authorName: 'Al Piepho',
   pathPrefix: '/state-web-development-2018-2019-withD3-pwa', // Note: it must *not* have a trailing slash.
 
